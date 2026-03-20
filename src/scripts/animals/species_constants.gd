1d725fe1c3c58b015047ed8b5ff3de6a6729ab74
Steven Gann <stevengannjr@gmail.com>
2026-03-19 17:57:32 -0700

    Tabula Nova

--- src/scripts/animals/species_constants.gd
+++
@@ -1,4 +0,0 @@
-extends RefCounted
-## Species IDs for animals. Must match BiologyGame.Animals.AnimalBase.Species (C#) for cross-script compatibility.
-## Used by WorldPopulator and GDScript code that references species.
-enum Species { BISON = 0, DEER = 1, RABBIT = 2, WOLF = 3, BEAR = 4 }
